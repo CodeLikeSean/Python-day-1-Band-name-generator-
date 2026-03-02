33acf428b67b7609d5fc79ec53b8e336ea989f65
Sean <104570261+CodeLikeSean@users.noreply.github.com>
2026-03-02 22:05:37 +0100

    Add files via upload

---
+++ task.py
@@ -0,0 +1,5 @@
+print("Welcome to the BAND NAME GENERATOR!")
+cityName = input("What city did you grow up in? ")
+petName = input("What is your pets name? ")
+
+print("Your epic band name is " + cityName + " " + petName + "!")
